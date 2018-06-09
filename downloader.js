2345c39dcc1e55a2ab8b101744afecb191567f20
Ruben Deyhle <deyhle@entrecode.de>
2018-06-09 08:25:12 +0200

    tried improving download logic

--- downloader.js
+++ downloader.js
@@ -5,28 +5,50 @@ const fsExtra = require('fs-extra');
 const map = new Map();
 const stack = new Set();
 
-async function download() {
-  if (!stack.size) {
-    return setTimeout(download, 1000);
-  }
-  const image = map.get([...stack].pop());
+async function downloadImage(image) {
   const response = await superagent.get(image.url)
-  .timeout({
-    response: 2000,
-    deadline: 30000,
-  })
-  .catch((e) => {
-    if (e.timeout || e.code === 'EHOSTDOWN' || e.code === 'ENOTFOUND' || e.code === 'EHOSTUNREACH') {
-      return;
-      //  throw new Error(`could not connect to ${this.host}`);
-    }
-    throw e;
-  });
+    .timeout({
+      response: 5000,
+      deadline: 10000,
+    })
+    .catch((e) => {
+      if (e.timeout || e.code === 'EHOSTDOWN' || e.code === 'ENOTFOUND' || e.code === 'EHOSTUNREACH') {
+        throw new Error(`could not connect to ${image.card.name} ${e}`);
+      }
+      throw e;
+    });
   await fsExtra.writeFile(`target/${image.card.name}/${image.filename}`, response.body);
   stack.delete(image.url);
   map.delete(image.url);
   eventstream.emit('downloaded', image);
-  download();
+}
+
+async function downloadInterval() {
+  if (!stack.size) {
+    return setTimeout(downloadInterval, 1000);
+  }
+  await (async () => {
+    const arrayStack = [...stack];
+    let image;
+    let success = false;
+    let lastError;
+    for (image = map.get(arrayStack.pop()); !success && arrayStack.length; image = map.get(arrayStack.pop())) {
+    //while (!success && (image = map.get(arrayStack.pop())) {
+      try {
+        await downloadImage(image);
+        success = true;
+      } catch (e) {
+        console.log(e);
+        lastError = e;
+      }
+    }
+    if (!success && lastError) {
+      throw lastError;
+    }
+  })().catch((e) => {
+    console.error(e);
+  });
+  downloadInterval();
 }
 
 function start() {
@@ -34,7 +56,7 @@ function start() {
     map.set(image.url, image);
     stack.add(image.url);
   });
-  download();
+  downloadInterval();
 }
 
 module.exports = { start };
